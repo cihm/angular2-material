f92076a8ca8ff960d4056b1bdfc9626d6f43f4da
lewisli <lewisli.acer@gmail.com>
2016-10-27 15:58:25 +0800

    mnodify port

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path: helpers.root('dist'),
-    publicPath: 'http://localhost:8080/a1/',
+    publicPath: 'http://localhost:4200/a1/',
     filename: '[name].js',
     chunkFilename: '[id].chunk.js'
   },
